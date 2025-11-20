97677f3d5e2a1b2630b83c8ea4168c65cf6fc0fe
muraliveesambattu <murali.veesambattu@gmail.com>
2025-11-20 09:43:29 +0530

    JS Functions Intro

---
+++ Javascript/JS1_Intro/intro.js
@@ -0,0 +1,38 @@
+function changeColor() {
+  document.querySelector("h2").style.color = "red";
+}
+// Primitive Data types
+
+// strings - A..Z
+// numbers - 0-9
+// Boolean - true or false
+// null
+// undefined
+
+
+
+var myName = "Murali";
+var myAge = 45;
+var haveVisa = true;
+var myNetworth = null;
+var myVehicle;
+
+// console.log(myName)
+// console.log(myAge)
+// console.log(haveVisa)
+// console.log(myNetworth)
+// console.log(myVehicle)
+
+// document.writeln(myName)
+// document.getElementById("demo").innerHTML = myName
+// alert(myName)
+// To Display the Values Stored In Variable 
+//     1. console.log 
+//     2. document.write
+//     3. document.getElementById("demo").innerHTML = "some"
+//     4. alert 
+
+// alert(myName)
+// var userInput = confirm("Hope you like JS")
+// console.log(userInput)
+// prompt("Please Enter your Name ")
